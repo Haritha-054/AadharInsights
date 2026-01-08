6151ff4b9850e89b223c82d4abb2fbc477272403
Dondeti Haritha Reddy <harithareddydondeti5454@gmail.com>
2026-01-08 16:54:51 +0530

    Backend fixes

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://aadharinsights-backend.onrender.com";
 
 export async function fetchSummary() {
   const res = await fetch(`${BASE_URL}/summary`);
@@ -8,12 +8,11 @@ export async function fetchSummary() {
 export async function fetchDistricts() {
   const res = await fetch(`${BASE_URL}/districts`);
   const data = await res.json();
-  console.log("District data received:", data); // DEBUG
+  console.log("District data received:", data);
   return data;
 }
 
-
 export const fetchFrictionAge = async () => {
-  const res = await fetch("http://127.0.0.1:8000/friction-age-analysis");
+  const res = await fetch(`${BASE_URL}/friction-age-analysis`);
   return res.json();
 };
